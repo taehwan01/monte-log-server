3824dacc9eec9f010f1d5c5b1f3252fceb6f6dc9
taehwan01 <monte6198@gmail.com>
2024-10-29 17:43:24 +0900

    feat: admin url 제거

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,13 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService); // ConfigService 가져오기
   const clientUrl = configService.get<string>('CLIENT_URL'); // 환경 변수에서 CLIENT_URL 추출
-  const adminClientUrl = configService.get<string>('ADMIN_CLIENT_URL'); // 환경 변수에서 ADMIN_CLIENT_URL 추출
 
   app.useGlobalFilters(new UnauthorizedRedirectFilter(configService));
 
   app.use(cookieParser());
 
   app.enableCors({
-    origin: [clientUrl, adminClientUrl], // Next.js가 실행되는 주소
+    origin: clientUrl, // Next.js가 실행되는 주소
     credentials: true, // 쿠키를 포함한 요청을 허용
     methods: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
   });
